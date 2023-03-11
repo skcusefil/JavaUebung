2d0dc7fc100e7ed8d72076c185ef9d3dcedd8816
LadyMisfortunate <80325540+LadyMisfortunate@users.noreply.github.com>
2023-03-11 22:10:58 +0100

    add Aufgabe1

---
+++ Aufgabe3/src/GenericBinaryTree.java
@@ -0,0 +1,61 @@
+
+//b) Erganzen Sie nun auch in der Klasse ¨ GenericBinaryTree eine Methode
+//public  boolean Genericcontains(T value)
+public class GenericBinaryTree<T extends Comparable<T>>{
+    class TreeNode{
+        final T value;
+        TreeNode left, right;
+        public  TreeNode(T value){
+            this.value = value;
+        }
+        public  void add(T value){
+            if(value.compareTo(this.value) > 0){
+                if(right == null) right = new TreeNode(value);
+                else right.add(value);
+            }else{
+                if(left == null) left = new TreeNode(value);
+                else left.add(value);
+            }
+        }
+
+        public  boolean contains(T value){
+            return  this.value == value;
+        }
+
+        @Override public String toString(){
+            String result = "";
+            if(left != null) result = left.toString();
+            result += " "+value;
+            if(right != null) result += right.toString();
+            return  result;
+        }
+    }
+    class Complex{
+        final TreeNode treeNode;
+        public Complex(TreeNode root){
+            this.treeNode = root;
+        }
+        @Override public String toString(){
+            String result = "";
+            if(treeNode != null) result = treeNode.toString();
+            return result;
+        }
+    }
+
+    TreeNode root = null;
+    Complex complex = null;
+
+    public  void add(T value){
+        if(root == null) root = new TreeNode(value);
+        else root.add(value);
+    }
+
+    public  boolean contains(T value){
+        return  root.contains(value);
+    }
+    @Override public  String toString() {
+        String result = "";
+        if(root != null) complex = new Complex(root);
+        return complex.toString();
+    }
+}
---
+++ Aufgabe3/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BinaryTreeFromAufgabe2 tree = new BinaryTreeFromAufgabe2<Integer>();
+        tree.add(2);
+        tree.add(10);
+        tree.add(5);
+        System.out.println(tree.contains(2));
+
+        GenericBinaryTree<String> tree2 = new GenericBinaryTree<String>();
+        tree2.add("aba");
+        tree2.add("zab");
+        tree2.add("ufo");
+
+        System.out.println(tree2);
+    }
+}
